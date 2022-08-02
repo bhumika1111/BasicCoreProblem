af026875703c86f416e029e420e3b91501ea58fc
bhumika1111 <bhoyarpatil@434>
2022-08-02 07:59:49 +0530

    Checking Table Of Power Of Two

---
+++ PowerOfTwo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicCorePrograms
+{
+    /// <summary>
+    /// Here We Check The Table Of Power Of Two.
+    /// </summary>
+    public static class PowerOfTwo
+    {
+        /// <summary>
+        /// Check Power Of Two.
+        /// </summary>
+        public static void CheckingPowerOfTwo()
+        {
+
+            Console.WriteLine("Input any value:");
+            int N = Convert.ToInt32(Console.ReadLine());
+
+            if (N >= 0 && N < 31)
+            {
+                Console.WriteLine("Entered the value within the range:  " + N);
+
+                for (int i = 1; i <= N; i++)
+                {
+                    Console.WriteLine(Math.Pow(2, i));
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Entered value is not in range ");
+            }
+        }
+    }
+}
