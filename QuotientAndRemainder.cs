372a44bdaef565771d32344911bf52a84f627ae8
bhumika1111 <bhoyarpatil@434>
2022-08-02 10:22:52 +0530

    Checking Quotient And Remainder Program

---
+++ QuotientAndRemainder.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicCorePrograms
+{
+    /// <summary>
+    /// Here We Check Quotient And Remainder.
+    /// </summary>
+    public static class QuotientAndRemainder
+    {
+        /// <summary>
+        /// Checking The Number.
+        /// </summary>
+        public static void CheckingQuotient()
+        {
+           
+        
+
+           Console.WriteLine("Enter A First Number:");
+           int  dividend = Convert.ToInt32(Console.ReadLine());
+           
+            Console.WriteLine("Enter A Second Number:");
+            int divisor = Convert.ToInt32(Console.ReadLine());
+
+            int quotient = dividend / divisor;
+            int remainder = dividend % divisor;
+
+            Console.WriteLine("dividend:{0} divisor: {1} ", dividend, divisor);
+            Console.WriteLine("Quotient = " + quotient);
+            Console.WriteLine("Remainder = " + remainder);
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
