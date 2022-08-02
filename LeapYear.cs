160c7abbc9a36435d2d1f80a85d7b3e089653964
bhumika1111 <bhoyarpatil@434>
2022-08-02 07:54:44 +0530

    Checking Leap Year

---
+++ LeapYear.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicCorePrograms
+{
+    /// <summary>
+    /// Here We Check The Year Is Leap Year Or Not.
+    /// </summary>
+   public static class LeapYear
+    {
+        /// <summary>
+        /// Check Leap Year.
+        /// </summary>
+        public static void CheckLeapYear()
+        {
+            Console.WriteLine("Enter Year is four digit Number");
+            int Year = Convert.ToInt32(Console.ReadLine());
+
+            if ((Year % 4 == 0) || (Year % 100 != 0) && (Year % 4 == 0))
+            {
+
+                Console.WriteLine("  Leap year  " + Year);
+            }
+
+            else
+            {
+                Console.WriteLine("  Not leap year  " + Year);
+            }
+        }
+    }
+}
