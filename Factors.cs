fddc78eaf6ca1ee6ca9c5fb93b00253c90d999d4
bhumika1111 <bhoyarpatil@434>
2022-08-02 10:19:55 +0530

    Checking Prime Factor

---
+++ Factors.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicCorePrograms
+{
+  public static class Factors
+    {
+        public static void CheckingPrimeFactorization()
+        {
+            int i, num, b;
+            
+            Console.WriteLine("Please Enter Any Number To Find Factors:");
+            num = Convert.ToInt32(Console.ReadLine());
+           
+            for (i = 2; i*i <= num; i++)
+            {
+                if(num % i == 0)
+                {
+                    b = 0;
+                    while (num % i == 0)
+                    {
+                        num /= i;
+                        b++;
+                    }
+                    Console.WriteLine($"{i} is a Prime Factor {b} Times!");
+                }
+
+            }
+              
+            
+        }
+  }
+}
+
