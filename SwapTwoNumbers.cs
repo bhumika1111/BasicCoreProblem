0629c2d2200919f1f7c2f3cc65ac6c7d9a14dd12
bhumika1111 <bhoyarpatil@434>
2022-08-02 10:25:10 +0530

    Check Swapping Of Two Numbers

---
+++ SwapTwoNumbers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicCorePrograms
+{
+    /// <summary>
+    ///  Here We Check Swapping Of Two Numbers Without Using Third Variable
+    /// </summary>
+    public static  class SwapTwoNumbers
+    {
+        public static void CheckSwapNumber()
+        {
+            int a = 50, b = 100;
+            Console.WriteLine("Before Swapping a = "  +  a + " b = "  + b);
+            a = a * b; a = 5000;
+            b = a / b; b = 50;
+            a = a / b;
+            Console.WriteLine(" After Swapping a = "  + a + " b = "  + b);
+
+        }
+    }
+}
