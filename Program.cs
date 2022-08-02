083331aef20841e786fe7a7b018960378addf720
bhumika1111 <bhoyarpatil@434>
2022-08-02 07:49:19 +0530

    Basic Core Problems

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace BasicCorePrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Console.WriteLine("******Output For Checking The Percentage Of Head And Tail******* ");
+            FlipTheCoin.CheckCoinFlip();
+            Console.WriteLine("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
+            Console.WriteLine("****Output For Checking The Year Is Leap Year Or Not");
+            LeapYear.CheckLeapYear();
+            Console.WriteLine(":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
+            Console.WriteLine("*****Output For Checking The Table Of Power Of To Upto 30********");
+            PowerOfTwo.CheckingPowerOfTwo();
+            Console.WriteLine("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
+            Console.WriteLine("********** Output For Checking Check  The Nth Harmonic Value.*********");
+            HarmonicNumber.CheckHarmonicSeries();
+            Console.WriteLine("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
+            Console.WriteLine("**********Output For Check Swapping Of Two Numbers.***********");
+            SwapTwoNumbers.CheckSwapNumber();
+            Console.WriteLine("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
+            Console.WriteLine("**********Output For  Check Wheather An Alphabet Is Vowel Or Consonant.********");
+            CheckVowel.CheckingAnAlphabet();
+            Console.WriteLine("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
+            Console.WriteLine("*******Output For Check Wheather A Number Is Even Or Odd.********");
+            EvenAndOdd.CheckingNumber();
+            Console.WriteLine("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
+            Console.WriteLine("********Output For Checking Quotient And Remainder***********");
+            QuotientAndRemainder.CheckingQuotient();
+            Console.WriteLine("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
+            Console.WriteLine("**********Output For Checking The Largest Three Number************");
+            LargestThreeNumber.CheckingLargestNumber();*/
+            Console.WriteLine("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
+            Console.WriteLine("**********Output For Checking The Prime Factor Number************");
+            Factors.CheckingPrimeFactorization();
+
+
+
+        }
+    }
+}
